439de5d11966370e73159462775ef22430d1053e
entiek205 <verybaomat1o1@gmail.com>
2026-08-04 14:52:06 +0700

    Update Question.cpp

---
+++ QuizSystem/src/Question.cpp
@@ -0,0 +1,14 @@
+#include "Question.h"
+
+using namespace std;
+Question::Question(int id, const string& prompt, int points)
+    : id(id), prompt(prompt), points(points) {
+}
+
+int Question::getId() const {
+    return id;
+}
+
+int Question::getPoints() const {
+    return points;
+}
