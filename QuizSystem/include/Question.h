ab7055621a0740b8b909e4497dcd3086a4fde7ee
entiek205 <verybaomat1o1@gmail.com>
2026-08-04 14:42:44 +0700

    Update Question.h

--- QuizSystem/include/Question.h
+++ QuizSystem/include/Question.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <string>
+
+class Question {
+
+protected:
+    int id;
+    std::string prompt;
+    int points;
+
+public:
+    Question(int id, const std::string& prompt, int points);
+    virtual ~Question() = default;
+    virtual void display() const = 0;
+
+    //TF,MCQ answer
+    virtual bool checkAnswer(const std::string& answer) const = 0;
+
+    //QuizEngine
+    int getId() const;
+    int getPoints() const;
+};
