3d7aab179cde94ddab0ccd347297ad9d2bb8cffa
entiek205 <verybaomat1o1@gmail.com>
2026-08-04 14:45:56 +0700

    Update TF.h

--- QuizSystem/include/TF.h
+++ QuizSystem/include/TF.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Question.h"
+#include <string>
+
+class TF : public Question {
+private:
+    bool correctAnswer;//true,false
+
+public:
+    TF(int id, const std::string& prompt, int points, bool correctAnswer);
+    void display() const override;
+    bool checkAnswer(const std::string& answer) const override;
+};
