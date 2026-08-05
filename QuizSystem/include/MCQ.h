b426734c6da76610936b3f4e3a573d614013c6c8
entiek205 <verybaomat1o1@gmail.com>
2026-08-05 14:37:33 +0700

    Update MCQ.h

--- QuizSystem/include/MCQ.h
+++ QuizSystem/include/MCQ.h
@@ -1,15 +1,16 @@
 #pragma once
-#include "../include/Question.h"
+#include "Question.h"
 #include <string>
+
 class MCQ : public Question {
 private:
-    std::string options[4]; //ABCD
+    std::string options;//fix từ char thành 1 string chứa ABCD
     char correctOption;
+
 public:
-    MCQ(int id, const std::string& prompt, int points,
-        const std::string& optA, const std::string& optB,
-        const std::string& optC, const std::string& optD,
-        char correctOption);
+    MCQ(int id, const std::string& prompt, int points, const std::string& options, char correctOption);
+
+    ~MCQ() override = default;
 
     void display() const override;
     bool checkAnswer(const std::string& answer) const override;
