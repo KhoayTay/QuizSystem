17f432cc4852bce7ba069ae01db29a45f149c2e8
entiek205 <verybaomat1o1@gmail.com>
2026-08-05 16:00:34 +0700

    Update MCQ.cpp

--- QuizSystem/src/MCQ.cpp
+++ QuizSystem/src/MCQ.cpp
@@ -1,19 +1,33 @@
 #include "../include/MCQ.h"
 #include <iostream>
-#include <cctype>
+#include <cctype>//cho hàm toupper kiểu in hoa-thường
+
 using namespace std;
-MCQ::MCQ(int id, const string& prompt, int points, const string& options, char correctOption)
-    : Question(id, prompt, points), options(options), correctOption(correctOption) {
+MCQ::MCQ(int id, const string& prompt, int points, const vector<string>& options, const string& correctOptionStr)
+    : Question(id, prompt, points), options(options) {
+    if (!correctOptionStr.empty()) {
+        correctOption = toupper(correctOptionStr[0]);
+    }
+    else {
+        correctOption = 'A';
+    }
 }
+
 void MCQ::display() const {
     cout << "Câu " << getId() << ": " << prompt << " (" << getPoints() << " diem)\n";
-    cout << options << "\n";
+    char label = 'A';
+    for (size_t i = 0; i < options.size(); i++) {
+        cout << label << ". " << options[i] << "   ";
+        label++;
+    }
+    cout << "\n";
 }
+
 bool MCQ::checkAnswer(const string& answer) const {
-    //không nhập gì sẽ tính là false
+
     if (answer.empty()) return false;
-    //So sánh Answer với char correctoption trả về gtri tương ứng
-    if (toupper(answer[0]) == toupper(correctOption)) {
+
+    if (toupper(answer[0]) == correctOption) {
         return true;
     }
     else {
