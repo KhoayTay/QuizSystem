418c6c3a0b3e8e1b511477014a247ed714d20e2c
entiek205 <verybaomat1o1@gmail.com>
2026-08-06 16:19:17 +0700

    Update MCQ.h

--- QuizSystem/include/MCQ.h
+++ QuizSystem/include/MCQ.h
@@ -6,12 +6,11 @@
 class MCQ : public Question {
 private:
     std::vector<std::string> options;
-    char correctOption;
 
 public:
-    MCQ(int id, const std::string& prompt, int points, const std::vector<std::string>& options, const std::string& correctOption);
+    MCQ(int id, const std::string& prompt, int points, const std::vector<std::string>& options, const std::string& correctOptionStr);
     ~MCQ() override = default;
 
     void display() const override;
-    bool checkAnswer(const std::string& answer) const override;
+    std::vector<std::string> getOptions() const;
 };
