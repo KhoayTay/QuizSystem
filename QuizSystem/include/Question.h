2f4c6817c5d3354bf6ef4a54fcb015edc234725e
entiek205 <verybaomat1o1@gmail.com>
2026-08-07 15:35:01 +0700

    Update Question.h

--- QuizSystem/include/Question.h
+++ QuizSystem/include/Question.h
@@ -12,10 +12,6 @@ public:
     Question(int id, const std::string& prompt, int points);
     virtual ~Question() = default;
     virtual void display() const = 0;
-
-    //TF,MCQ answer
-    virtual bool checkAnswer(const std::string& answer) const = 0;
-
     //Getter
     int getId() const;
     int getPoints() const;
