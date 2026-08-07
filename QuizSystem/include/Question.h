2b6958b087b779b6d2f1c54892712f72a260d7da
entiek205 <verybaomat1o1@gmail.com>
2026-08-07 17:25:39 +0700

    Update Question.h

--- QuizSystem/include/Question.h
+++ QuizSystem/include/Question.h
@@ -1,15 +1,17 @@
 #pragma once
 #include <string>
+#include <vector>
 
 class Question {
 
 protected:
     int id;
-    std::string prompt;
     int points;
+    std::string prompt;
+    std::string correctAnswer;
 
 public:
-    Question(int id, const std::string& prompt, int points);
+    Question(int id, const std::string& prompt, int points, const std::string& correctAnswer);
     virtual ~Question() = default;
     virtual void display() const = 0;
     //Getter
