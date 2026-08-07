01e7619b856708f77032b6f316b44186c072f1ab
entiek205 <verybaomat1o1@gmail.com>
2026-08-07 15:38:33 +0700

    Update TF.cpp

--- QuizSystem/src/TF.cpp
+++ QuizSystem/src/TF.cpp
@@ -4,41 +4,9 @@
 
 using namespace std;
 TF::TF(int id, const string& prompt, int points, const string& correctAnswerStr)
-    : Question(id, prompt, points) {
+    : Question(id, prompt, points, correctAnswerStr) {}
 
-    // Ép kiểu từ string sang bool an toàn
-    if (!correctAnswerStr.empty() && (toupper(correctAnswerStr[0]) == 'T' || correctAnswerStr == "1")) {
-        correctAnswer = true;
-    }
-    else {
-        correctAnswer = false;
-    }
-}
 void TF::display() const {
     cout << "Câu " << getId() << ": " << prompt << " (" << getPoints() << " diem)\n";
     cout << "[ Vui long chon True (T) hoac False (F) ]\n";
-}
-
-bool TF::checkAnswer(const string& answer) const {
-    // Không nhập gì sẽ tính là false
-    if (answer.empty()) {
-        return false;
-    }
-
-    // Lấy ký tự đầu tiên của câu trả lời và chuyển thành chữ in hoa
-    char firstLetter = toupper(answer[0]);
-    bool userAnswerBool;
-    // So sanh T với F để xác định true false
-    if (firstLetter == 'T' || firstLetter == '1') {
-        userAnswerBool = true;
-    }
-    else {
-        userAnswerBool = false;
-    }
-    if (userAnswerBool == correctAnswer) {
-        return true;
-    }
-    else {
-        return false;
-    }
 }
